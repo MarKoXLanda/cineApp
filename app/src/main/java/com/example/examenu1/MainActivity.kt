29199cf9fb8950371aebd101528ffdf9b101e870
MarkoLandia <marko_fer_rodriguez@hotmail.com>
2026-09-25 12:14:13 -0600

    first commit

---
+++ app/src/main/java/com/example/examenu1/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.examenu1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.examenu1.ui.theme.ExamenU1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ExamenU1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+//Crear un proyecto y publicar en Gitjub
+/*
+Realizar CineApp
+Tiene que tener en columna y reenglones
+Cada caja para cada pelicula con la capacidad de scroll
+Imagen con texto osea resumen de la pelicula, Al menos 10 para notar scroll
+*/
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ExamenU1Theme {
+        Greeting("Android")
+    }
+}
